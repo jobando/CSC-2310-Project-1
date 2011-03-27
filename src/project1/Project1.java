8af1de1bf8bfc8e1d6d147ce6afeae970ede28be
unknown <eltico@.(none)>
2011-03-27 19:43:20 -0400

    Initial repository. Basic frame.

---
+++ src/project1/Project1.java
@@ -0,0 +1,26 @@
+package project1;
+
+import java.util.Hashtable;
+import processing.core.*;
+
+/**
+ *
+ * @author eltico
+ */
+public class Project1 extends PApplet {
+    
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
