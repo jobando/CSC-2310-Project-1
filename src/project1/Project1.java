6aa38bfc1628a52887161d7d07571d4e48e56cdf
unknown <eltico@.(none)>
2011-03-27 20:13:42 -0400

    Adding the main method procedure

--- src/project1/Project1.java
+++ src/project1/Project1.java
@@ -12,19 +12,27 @@ public class Project1 extends PApplet {
     private static Hashtable params = new Hashtable(); //Hash table. It will save the parameters from the args
     private int width, height; //Dimensions for the window
 
-
     @Override
     public void setup() {
     }
 
     @Override
     public void draw() {
-
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String[] newArgs = new String[args.length + 1]; 
+        newArgs[0] = "project1.Project1";
+        params.put("height", args[1]);
+        params.put("width", args[0]);
+        //Stores the array args into the hashtable so it can be processed during excution of the PApplet.
+        for (int i = 2; i < args.length; i++) {
+            newArgs[i + 1] = args[i];
+            params.put("Bar" + i, args[i]);
+        }
+        PApplet.main(newArgs);
     }
 }
