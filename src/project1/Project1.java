aacce5ac2064bc1f08ee857d9717a73f270c05ac
unknown <eltico@.(none)>
2011-03-27 20:37:58 -0400

    Decided against hash table and went for the array to store the arguments array.

--- src/project1/Project1.java
+++ src/project1/Project1.java
@@ -1,5 +1,6 @@
 package project1;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
 import processing.core.*;
 
@@ -9,18 +10,13 @@ import processing.core.*;
  */
 public class Project1 extends PApplet {
 
-    private static Hashtable params = new Hashtable(); //Hash table. It will save the parameters from the args
-    private int width, height; //Dimensions for the window
-
-    @Override
-    public String param(String id) {
-        return (String) params.get(id);
-    }
+    private static String[] arguments; //Arg copy from the main method.
+    private int width, height; //Dimensions for the window.
 
     @Override
     public void setup() {
-        width = Integer.parseInt(param("width"));
-        height = Integer.parseInt(param("height"));
+        width = Integer.parseInt(arguments[0]);
+        height = Integer.parseInt(arguments[1]);
         size(height, width);
         smooth();
     }
@@ -34,15 +30,8 @@ public class Project1 extends PApplet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] newArgs = new String[args.length + 1];
-        newArgs[0] = "project1.Project1";
-        params.put("height", args[1]);
-        params.put("width", args[0]);
-        //Stores the array args into the hashtable so it can be processed during excution of the PApplet.
-        for (int i = 2; i < args.length; i++) {
-            newArgs[i + 1] = args[i];
-            params.put("Bar" + i, args[i]);
-        }
-        PApplet.main(newArgs);
+
+        arguments = args;
+        PApplet.main(new String[]{"project1.Project1"});
     }
 }
